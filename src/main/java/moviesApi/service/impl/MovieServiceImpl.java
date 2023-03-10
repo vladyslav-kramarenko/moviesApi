30566cb14c3d633cac423f76c1fa7c2ef760dba3
Vladyslav Kramarenko <razorvlad1992@gmail.com>
2023-03-10 14:51:16 -0800

    added pagination, some refactoring

--- src/main/java/moviesApi/service/impl/MovieServiceImpl.java
+++ src/main/java/moviesApi/service/impl/MovieServiceImpl.java
@@ -6,10 +6,10 @@ import moviesApi.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class MovieServiceImpl implements MovieService {
@@ -42,23 +42,25 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public long count(String genre, Integer year, Long directorId, Long actorId) {
-        return movieRepository.findAll().stream()
-                .filter(movie -> (genre == null || movie.getGenre().equalsIgnoreCase(genre)))
-                .filter(movie -> (year == null || movie.getReleaseYear().equals(year)))
-                .filter(movie -> (directorId == null || movie.getDirectorId().equals(directorId)))
-                .filter(movie -> (actorId == null || movie.getActorIds().contains(actorId)))
-                .count();
+        return filter(movieRepository.findAll().stream(), genre, year, directorId, actorId).count();
     }
 
     @Override
     public List<Movie> filterMovies(String genre, Integer year, Long directorId, Long actorId, Pageable pageable) {
-        List<Movie> allMovies = movieRepository.findAll();
 
-        return allMovies.stream()
-                .filter(movie -> (genre == null || movie.getGenre().equalsIgnoreCase(genre)))
+        Stream<Movie> movieStream = movieRepository.findAll().stream();
+        movieStream = filter(movieStream, genre, year, directorId, actorId);
+
+        return movieStream
+                .skip(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .collect(Collectors.toList());
+    }
+
+    private Stream<Movie> filter(Stream<Movie> input, String genre, Integer year, Long directorId, Long actorId) {
+        return input.filter(movie -> (genre == null || movie.getGenre().equalsIgnoreCase(genre)))
                 .filter(movie -> (year == null || movie.getReleaseYear().equals(year)))
                 .filter(movie -> (directorId == null || movie.getDirectorId().equals(directorId)))
-                .filter(movie -> (actorId == null || movie.getActorIds().contains(actorId)))
-                .collect(Collectors.toList());
+                .filter(movie -> (actorId == null || movie.getActorIds().contains(actorId)));
     }
 }
